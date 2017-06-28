1b1678aa06c630152e991d9355258216ffde887d
Josep Jaume Rey <josepjaume@gmail.com>
2017-06-28 10:24:11 +0200

    Update installation.ex

--- lib/decidim_monitor/api/installation.ex
+++ lib/decidim_monitor/api/installation.ex
@@ -11,7 +11,8 @@ defmodule DecidimMonitor.Api.Installation do
     "staging" => %{ name: "Decidim Staging", url: "http://staging.decidim.codegram.com" },
     "pamplona" => %{ name: "Erabaki Pamplona", url: "https://erabaki.pamplona.es" },
     "mataro" => %{ name: "Decidim Mataró", url: "https://www.decidimmataro.cat" },
-    "diba" => %{ name: "Decidim Diputació de Barcelona", url: "http://decidim.diba.cat" }
+    "diba" => %{ name: "Decidim Diputació de Barcelona", url: "http://decidim.diba.cat" },
+    "badalona" => %{ name: "Decidim Badalona", url: "http://decidim.badalona.cat" }
   }
 
   @graphql_query "{ decidim { version } }"
