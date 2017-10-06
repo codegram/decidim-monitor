2c5a0d44bc3aa5bb2f5ff75b43351e96be6ba61f
Marc Riera Casals <mrc2407@gmail.com>
2017-10-06 09:53:39 +0200

    Add installation repo

--- lib/decidim_monitor/api/installation.ex
+++ lib/decidim_monitor/api/installation.ex
@@ -1,19 +1,19 @@
 defmodule DecidimMonitor.Api.Installation do
   @installations %{
-    "barcelona" => %{name: "Decidim Barcelona", url: "https://www.decidim.barcelona", codegram: true },
-    "hospitalet" => %{ name: "L'H-ON Participa", url: "https://www.lhon-participa.cat", codegram: true },
-    "terrassa" => %{ name: "Decidim Terrassa", url: "https://participa.terrassa.cat", codegram: true },
-    "sabadell" => %{ name: "Decidim Sabadell", url: "https://decidim.sabadell.cat", codegram: true },
-    "gava" => %{ name: "Decidim Gavà", url: "https://participa.gavaciutat.cat", codegram: false },
-    "sant_cugat" => %{ name: "Decidim Sant Cugat", url: "https://decidim.santcugat.cat/", codegram: true },
-    "localret" => %{ name: "Decidim Localret", url: "http://decidim.localret.codegram.com", codegram: true },
-    "vilanova" => %{ name: "Vilanova Participa", url: "http://participa.vilanova.cat", codegram: false },
-    "staging" => %{ name: "Decidim Staging", url: "http://staging.decidim.codegram.com", codegram: true },
-    "pamplona" => %{ name: "Erabaki Pamplona", url: "https://erabaki.pamplona.es", codegram: false },
-    "mataro" => %{ name: "Decidim Mataró", url: "https://www.decidimmataro.cat", codegram: false },
-    "diba" => %{ name: "Decidim Diputació de Barcelona", url: "http://decidim.diba.cat", codegram: false },
-    "badalona" => %{ name: "Decidim Badalona", url: "https://decidim.badalona.cat", codegram: true },
-    "cndp" => %{ name: "Commission Nationale du Débat Public", url: "https://cndp.opensourcepolitics.eu", codegram: false }
+    "barcelona" => %{name: "Decidim Barcelona", url: "https://www.decidim.barcelona", codegram: true, repo: "https://github.com/AjuntamentdeBarcelona/decidim-barcelona/" },
+    "hospitalet" => %{ name: "L'H-ON Participa", url: "https://www.lhon-participa.cat", codegram: true, repo: "https://github.com/HospitaletDeLlobregat/decidim-hospitalet/" },
+    "terrassa" => %{ name: "Decidim Terrassa", url: "https://participa.terrassa.cat", codegram: true, repo: "https://github.com/AjuntamentDeTerrassa/decidim-terrassa/" },
+    "sabadell" => %{ name: "Decidim Sabadell", url: "https://decidim.sabadell.cat", codegram: true, repo: "https://github.com/AjuntamentDeSabadell/decidim-sabadell/" },
+    "gava" => %{ name: "Decidim Gavà", url: "https://participa.gavaciutat.cat", codegram: false, repo: "https://github.com/AjuntamentDeGava/decidim-gava/" },
+    "sant_cugat" => %{ name: "Decidim Sant Cugat", url: "https://decidim.santcugat.cat/", codegram: true, repo: "https://github.com/AjuntamentdeSantCugat/decidim-sant_cugat/" },
+    "localret" => %{ name: "Decidim Localret", url: "http://decidim.localret.codegram.com", codegram: true, repo: "https://github.com/codegram/decidim-localret/" },
+    "vilanova" => %{ name: "Vilanova Participa", url: "http://participa.vilanova.cat", codegram: false, repo: "https://github.com/vilanovailageltru/decidim-vilanova/" },
+    "staging" => %{ name: "Decidim Staging", url: "http://staging.decidim.codegram.com", codegram: true, repo: "" },
+    "pamplona" => %{ name: "Erabaki Pamplona", url: "https://erabaki.pamplona.es", codegram: false, repo: "https://github.com/ErabakiPamplona/erabaki/" },
+    "mataro" => %{ name: "Decidim Mataró", url: "https://www.decidimmataro.cat", codegram: false, repo: "https://github.com/AjuntamentDeMataro/decidim-mataro/" },
+    "diba" => %{ name: "Decidim Diputació de Barcelona", url: "http://decidim.diba.cat", codegram: false, repo: "https://github.com/diputacioBCN/decidim-diba/" },
+    "badalona" => %{ name: "Decidim Badalona", url: "https://decidim.badalona.cat", codegram: true, repo: "https://github.com/AjuntamentdeBadalona/decidim-badalona/" },
+    "cndp" => %{ name: "Commission Nationale du Débat Public", url: "https://cndp.opensourcepolitics.eu", codegram: false, repo: "" }
   }
 
   @graphql_query "{ decidim { version } }"
@@ -34,6 +34,7 @@ defmodule DecidimMonitor.Api.Installation do
     %{
       id: id,
       url: installation[:url],
+      repo: installation[:repo],
       name: installation[:name],
       version: remote_data["version"],
       status: remote_data["status"],
