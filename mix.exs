224f601bab9e3f5f4e453303306af976e39a4019
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-11-08 11:29:16 +0100

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -29,15 +29,14 @@ defmodule DecidimMonitor.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3.0-rc"},
+    [{:phoenix, "~> 1.3.0"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.2"},
-     {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:absinthe_plug, "~> 1.3.0-beta.1"},
+     {:absinthe_plug, "~> 1.3.1"},
      {:tesla, "~> 0.9.0"},
      {:poison, "~> 3.0"}]
   end
