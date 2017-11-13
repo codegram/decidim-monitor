ef14314ade204136ada7e13089381f60f85869d7
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-11-13 20:46:18 +0100

    Use different colors for different update status

--- frontend/src/app/installation/installation.component.ts
+++ frontend/src/app/installation/installation.component.ts
@@ -1,20 +1,39 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { map, startWith } from 'rxjs/operators';
-import compareVersion from 'node-version-compare';
+import semver from 'semver';
 
 @Component({
   selector: 'app-installation',
   templateUrl: './installation.component.html',
-  styleUrls: ['./installation.component.scss']
+  styleUrls: []
 })
 export class InstallationComponent implements OnInit {
   @Input() installation: any;
-  outdated: boolean;
-  outdatedColor: string;
+  color: string;
 
   ngOnInit() {
-    this.outdated = (compareVersion(this.installation.version, this.installation.currentVersion) < 0);
-    this.outdatedColor = this.outdated ? 'warn' : 'primary';
+    let version = this.installation.version;
+    let currentVersion = this.installation.currentVersion;
+
+    try {
+      let major = semver.major(version);
+      let minor = semver.minor(version);
+      let patch = semver.patch(version);
+
+      let currentMajor = semver.major(currentVersion);
+      let currentMinor = semver.minor(currentVersion);
+      let currentPatch = semver.patch(currentVersion);
+
+      if(semver.satisfies(`${major}.${minor}.${patch}`, `=${currentMajor}.${currentMinor}.${currentPatch}`)) {
+        this.color = "primary";
+      } else if(semver.satisfies(`${major}.${minor}.${patch}`, `>=${currentMajor}.${currentMinor}.x`)) {
+        this.color = "accent";
+      } else {
+        this.color = "warn";
+      }
+    } catch(e) {
+      this.color = "warn";
+    }
   }
 }
