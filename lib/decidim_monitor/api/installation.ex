a2a7088a007b3615018c790ce73ae1a741e79456
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2018-03-23 09:07:21 -0300

    Adds Generalitat de Catalunya

--- lib/decidim_monitor/api/installation.ex
+++ lib/decidim_monitor/api/installation.ex
@@ -166,6 +166,11 @@ defmodule DecidimMonitor.Api.Installation do
       name: "Malgrat Decideix",
       url: "https://decideix.ajmalgrat.cat/",
       codegram: false
+    },
+    "gencat" => %{
+      name: "Generalitat de Catalunya",
+      url: "https://participa.gencat.cat/",
+      codegram: false
     }
   }
 
